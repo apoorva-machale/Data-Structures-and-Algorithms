adbe99ed40f06538f8920bf7b7757e7d08275728
Apoorva Machale <apoorvaa.machale@gmail.com>
2023-06-20 20:57:11 -0700

    1. added inorder traversal without using recursion

--- binary_tree.cpp
+++ binary_tree.cpp
@@ -1,15 +1,13 @@
 // Tree traversal in C++
 
 #include <iostream>
+#include <stack>
+#include <vector>
 using namespace std;
 
 struct Node {
   int data;
   struct Node *left, *right;
-  /*Node(int data) {
-    this->data = data;
-    left = right = NULL;
-  }*/
 };
 
 // Preorder traversal
@@ -42,6 +40,29 @@ void inorderTraversal(struct Node* node) {
   inorderTraversal(node->right);
 }
 
+void inorder_wo_recursion(struct Node* node) {
+	if (node == NULL)
+    		return;
+	stack<Node*> stk;
+        vector<int> path;
+        Node* current = node;
+
+        while (!stk.empty() || current != NULL) {
+            while (current != NULL) {
+                stk.push(current);
+                current = current->left;
+            }
+            Node* temp = stk.top();
+            path.push_back(temp->data);
+            stk.pop();
+            current = temp->right;
+        }
+        for (int i = 0; i < path.size(); i++) {
+          cout<< path[i] << "->";
+        }
+ 
+}
+
 void insert(Node* root, Node* pnew){
 	char ch;
 	cout<<"your choice = l or r";
@@ -63,7 +84,9 @@ void insert(Node* root, Node* pnew){
 }
 
 Node* create(Node* root){
- int n=5,x;
+ int n,x;
+ cout<<"Enter number of nodes";
+ cin>>n;
 
  while(n){
  	Node* pnew = new Node;
@@ -86,9 +109,12 @@ int main() {
   Node* root = NULL;
   
   root = create(root);
-
+ 	
   cout << "Inorder traversal ";
   inorderTraversal(root);
+  
+  cout << "\nInorder traversal without recursion ";
+  inorder_wo_recursion(root);
 
   cout << "\nPreorder traversal ";
   preorderTraversal(root);
@@ -106,8 +132,22 @@ int main() {
 /*
 apoorva@ubuntu:~$ cd Documents/
 apoorva@ubuntu:~/Documents$ cd cpp/
-apoorva@ubuntu:~/Documents/cpp$ g++ -o bi_tree binary_tree.cpp 
-apoorva@ubuntu:~/Documents/cpp$ ./bi_tree 
-Inorder traversal 5->12->6->1->9->
-Preorder traversal 1->12->5->6->9->
-Postorder traversal 5->6->12->9->1->*/
+apoorva@ubuntu:~/Documents/cpp$ cd Binary\ Tree/
+apoorva@ubuntu:~/Documents/cpp/Binary Tree$ g++ -o binary binary_tree.cpp 
+apoorva@ubuntu:~/Documents/cpp/Binary Tree$ ./binary
+Enter number of nodes5
+Node number3
+Node number7
+your choice = l or rl
+Node number7
+your choice = l or rr
+Node number9
+your choice = l or rl
+your choice = l or rr
+Node number2
+your choice = l or rl
+your choice = l or rl
+Inorder traversal 2->7->9->3->7->
+Inorder traversal without recursion 2->7->9->3->7->
+Preorder traversal 3->7->2->9->7->
+Postorder traversal 2->9->7->7->3-> */
