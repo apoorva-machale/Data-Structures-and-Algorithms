ba275e7340aa9bac58e9a2d4e935660e58ec47bc
Apoorva Machale <apoorvaa.machale@gmail.com>
2023-06-20 21:43:34 -0700

    2. Added preorder and postorder traversals without using recursion

--- binary_tree.cpp
+++ binary_tree.cpp
@@ -20,6 +20,24 @@ void preorderTraversal(struct Node* node) {
   preorderTraversal(node->right);
 }
 
+// Preorder traversal without recursion
+void preorder_wo_recursion(struct Node* node){
+	if (node == NULL)
+    		return;
+	Node* p = node;
+	stack<Node*> s;
+	s.push(p);
+	while(!s.empty()){
+	        p = s.top();
+                s.pop();
+		cout<<p->data <<"->";
+		if(p->right !=NULL)
+			s.push(p->right);
+		if(p->left !=NULL)
+			s.push(p->left);
+	}
+}
+
 // Postorder traversal
 void postorderTraversal(struct Node* node) {
   if (node == NULL)
@@ -28,6 +46,31 @@ void postorderTraversal(struct Node* node) {
   postorderTraversal(node->left);
   postorderTraversal(node->right);
   cout << node->data << "->";
+}
+
+// Postorder traversal without recursion
+void postorder_wo_recursion(struct Node* node){
+	if (node == NULL)
+    		return;
+	Node* p = node;
+	stack<Node*> s1,s2;
+	s1.push(p);
+	while(!s1.empty()){
+		p = s1.top();
+                s1.pop();
+		//p = s1.pop(); In C++ the method, it doesn't return the value removed from the stack. The reason is that there's no way in general to write such a function correctly from an exception-safety point of view.You need to store the value first and then remove it with pop... e.g.
+		s2.push(p);
+		if(p->left !=NULL)
+			s1.push(p->left);
+		if(p->right !=NULL)
+			s1.push(p->right);
+	}
+	while(!s2.empty()){
+		p = s2.top();
+                s2.pop();
+		//p = s2.pop();
+		cout<<p->data <<"->";
+	}
 }
 
 // Inorder traversal
@@ -40,6 +83,7 @@ void inorderTraversal(struct Node* node) {
   inorderTraversal(node->right);
 }
 
+// Inorder traversal without recursion
 void inorder_wo_recursion(struct Node* node) {
 	if (node == NULL)
     		return;
@@ -118,9 +162,15 @@ int main() {
 
   cout << "\nPreorder traversal ";
   preorderTraversal(root);
+  
+  cout << "\nPreorder traversal without recursion ";
+  preorder_wo_recursion(root);
 
   cout << "\nPostorder traversal ";
   postorderTraversal(root);
+  
+  cout << "\nPostorder traversal without recursion ";
+  postorder_wo_recursion(root);
 
   return 0;
   
@@ -136,18 +186,19 @@ apoorva@ubuntu:~/Documents/cpp$ cd Binary\ Tree/
 apoorva@ubuntu:~/Documents/cpp/Binary Tree$ g++ -o binary binary_tree.cpp 
 apoorva@ubuntu:~/Documents/cpp/Binary Tree$ ./binary
 Enter number of nodes5
-Node number3
 Node number7
+Node number4
+your choice = l or r9
+Invalid characterNode number4
 your choice = l or rl
-Node number7
+Node number4
 your choice = l or rr
-Node number9
+Node number0
 your choice = l or rl
 your choice = l or rr
-Node number2
-your choice = l or rl
-your choice = l or rl
-Inorder traversal 2->7->9->3->7->
-Inorder traversal without recursion 2->7->9->3->7->
-Preorder traversal 3->7->2->9->7->
-Postorder traversal 2->9->7->7->3-> */
+Inorder traversal 4->0->7->4->
+Inorder traversal without recursion 4->0->7->4->
+Preorder traversal 7->4->0->4->
+Preorder traversal without recursion 7->4->0->4->
+Postorder traversal 0->4->4->7->
+Postorder traversal without recursion 0->4->4->7->*/
