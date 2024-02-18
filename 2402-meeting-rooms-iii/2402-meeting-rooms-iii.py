51c0b08808706274ab146acb993e1a9a24c3e50e
Apoorva Machale <96104516+apoorvaa-m@users.noreply.github.com>
2024-02-18 13:38:01 -0800

    Time: 1211 ms (55.02%), Space: 62.4 MB (77.54%) - LeetHub

--- 2402-meeting-rooms-iii/2402-meeting-rooms-iii.py
+++ 2402-meeting-rooms-iii/2402-meeting-rooms-iii.py
@@ -1,23 +1,20 @@
 class Solution:
     def mostBooked(self, n: int, meetings: List[List[int]]) -> int:
-        room_availability_time = [0] * n
+        unused_rooms, used_rooms = list(range(n)), []
+        heapify(unused_rooms)
         meeting_count = [0] * n
         for start, end in sorted(meetings):
-            min_room_availability_time = inf
-            min_available_time_room = 0
-            found_unused_room = False
-            for i in range(n):
-                if room_availability_time[i] <= start:
-                    found_unused_room = True
-                    meeting_count[i] += 1
-                    room_availability_time[i] = end
-                    break
-                if min_room_availability_time > room_availability_time[i]:
-                    min_room_availability_time = room_availability_time[i]
-                    min_available_time_room = i
-            if not found_unused_room:
-                room_availability_time[min_available_time_room] += end - start
-                meeting_count[min_available_time_room] += 1
-
+            while used_rooms and used_rooms[0][0] <= start:
+                _, room = heappop(used_rooms)
+                heappush(unused_rooms, room)
+            if unused_rooms:
+                room = heappop(unused_rooms)
+                heappush(used_rooms, [end, room])
+            else:
+                room_availability_time, room = heappop(used_rooms)
+                heappush(
+                    used_rooms,
+                    [room_availability_time + end - start, room]
+                )
+            meeting_count[room] += 1
         return meeting_count.index(max(meeting_count))
-        
