886ab0725ce17778c4864e99b53c7d928a6ef7b3
Apoorva Machale <96104516+apoorva-machale@users.noreply.github.com>
2024-04-01 12:57:36 -0700

    Time: 65 ms (97.27%), Space: 17.8 MB (84.32%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.py
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.py
@@ -1,28 +1,42 @@
 class Solution:
     def searchRange(self, nums: List[int], target: int) -> List[int]:
-        def binary(nums, target, key_part):
-            keyIndex = -1
-            start = 0
-            end = len(nums) - 1
-            while start<=end:
-                mid = start + (end-start) // 2
-                if nums[mid] < target:
-                    start=mid+1
-                elif nums[mid] > target:
-                    end=mid-1
+        
+        lower_bound = self.findBound(nums, target, True)
+        if (lower_bound == -1):
+            return [-1, -1]
+        
+        upper_bound = self.findBound(nums, target, False)
+        
+        return [lower_bound, upper_bound]
+        
+    def findBound(self, nums: List[int], target: int, isFirst: bool) -> int:
+        
+        N = len(nums)
+        begin, end = 0, N - 1
+        while begin <= end:
+            mid = int((begin + end) / 2)    
+            
+            if nums[mid] == target:
+                
+                if isFirst:
+                    # This means we found our lower bound.
+                    if mid == begin or nums[mid - 1] < target:
+                        return mid
+
+                    # Search on the left side for the bound.
+                    end = mid - 1
                 else:
-                    keyIndex = mid
-                    if key_part:
-                        start = mid+1
-                    else:
-                        end=mid-1
-            return keyIndex
+                    
+                    # This means we found our upper bound.
+                    if mid == end or nums[mid + 1] > target:
+                        return mid
+                    
+                    # Search on the right side for the bound.
+                    begin = mid + 1
+            
+            elif nums[mid] > target:
+                end = mid - 1
+            else:
+                begin = mid + 1
         
-        result = [-1,-1]
-        result[0] = binary(nums, target, False)
-        if result[0] != -1:
-            result[1] = binary(nums, target, True)
-            return result
-        return [-1, -1]
-    
-        
+        return -1
