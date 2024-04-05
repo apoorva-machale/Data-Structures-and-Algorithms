2529756adfbb61c72c8010189102da69e1aeb369
Apoorva Machale <96104516+apoorva-machale@users.noreply.github.com>
2024-04-05 15:52:27 -0700

    Time: 31 ms (81.14%), Space: 16.7 MB (6.89%) - LeetHub

--- 0024-swap-nodes-in-pairs/0024-swap-nodes-in-pairs.py
+++ 0024-swap-nodes-in-pairs/0024-swap-nodes-in-pairs.py
@@ -5,27 +5,17 @@
 #         self.next = next
 class Solution:
     def swapPairs(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        # Dummy node acts as the prevNode for the head node
-        # of the list and hence stores pointer to the head node.
-        dummy = ListNode(-1)
-        dummy.next = head
+        # If the list has no node or has only one node left.
+        if not head or not head.next:
+            return head
 
-        prev_node = dummy
+        # Nodes to be swapped
+        first_node = head
+        second_node = head.next
 
-        while head and head.next:
+        # Swapping
+        first_node.next  = self.swapPairs(second_node.next)
+        second_node.next = first_node
 
-            # Nodes to be swapped
-            first_node = head;
-            second_node = head.next;
-
-            # Swapping
-            prev_node.next = second_node
-            first_node.next = second_node.next
-            second_node.next = first_node
-
-            # Reinitializing the head and prev_node for next swap
-            prev_node = first_node
-            head = first_node.next
-
-        # Return the new head node.
-        return dummy.next
+        # Now the head is the second node
+        return second_node
