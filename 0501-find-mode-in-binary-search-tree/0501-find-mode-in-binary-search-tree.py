d3ee5bba7e7f53d9ff462da3e93980d3540b7c16
Apoorva Machale <96104516+apoorva-machale@users.noreply.github.com>
2024-04-08 15:54:48 -0700

    Time: 40 ms (89.52%), Space: 19.7 MB (90.13%) - LeetHub

--- 0501-find-mode-in-binary-search-tree/0501-find-mode-in-binary-search-tree.py
+++ 0501-find-mode-in-binary-search-tree/0501-find-mode-in-binary-search-tree.py
@@ -6,35 +6,32 @@
 #         self.right = right
 class Solution:
     def findMode(self, root: Optional[TreeNode]) -> List[int]:
-        def dfs(node, values):
+        def dfs(node):
+            nonlocal max_streak, curr_streak, curr_num, ans
             if not node:
                 return
             
-            # Inorder traversal visits nodes in sorted order
-            dfs(node.left, values)
-            values.append(node.val)
-            dfs(node.right, values)
+            dfs(node.left)
             
-        values = []
-        dfs(root, values)
-        
-        max_streak = 0
-        curr_streak = 0
-        curr_num = 0
-        ans = []
-        
-        for num in values:
+            num = node.val
             if num == curr_num:
                 curr_streak += 1
             else:
                 curr_streak = 1
                 curr_num = num
-                
+
             if curr_streak > max_streak:
                 ans = []
                 max_streak = curr_streak
 
             if curr_streak == max_streak:
                 ans.append(num)
+            
+            dfs(node.right)
 
+        max_streak = 0
+        curr_streak = 0
+        curr_num = 0
+        ans = []
+        dfs(root)
         return ans
